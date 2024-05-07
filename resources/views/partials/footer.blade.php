b38af5e49cc0dbfe44579cac0cc3a53c480d91e8
marinodilauro <m.dilauro90@gmail.com>
2024-05-07 18:40:47 +0200

    Add footer top section

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,86 @@
-<footer>
+<footer id="footer">
 
-  Footer
+  <div id="footer_top">
+    <div class="container wrapper">
+      <div class="row">
+
+        <div class="col-6">
+          <div class="container d-flex">
+            <div class="row justify-content-between row-cols-3 g-4">
+
+              <div class="col ps-0">
+                <h4>DC COMICS</h4>
+                <ul class="list-unstyled">
+                  <li>Charachter</li>
+                  <li>Comics</li>
+                  <li>Movies</li>
+                  <li>TV</li>
+                  <li>Games</li>
+                  <li>Videos</li>
+                  <li>News</li>
+                </ul>
+
+                <h4>SHOP</h4>
+                <ul class="list-unstyled">
+                  <li>Shop DC</li>
+                  <li>Shop DC Collectibles</li>
+                </ul>
+
+              </div>
+
+              <div class="col">
+                <h4>DC</h4>
+                <ul class="list-unstyled">
+                  <li>Term Of Use</li>
+                  <li>Privacy Policy (New)</li>
+                  <li>Ad Choices</li>
+                  <li>Advertising</li>
+                  <li>Jobs</li>
+                  <li>Subscriptions</li>
+                  <li>Talent Workshop</li>
+                  <li>CPSC Certificates</li>
+                  <li>Ratings</li>
+                  <li>Shop Help</li>
+                  <li>Contact Us</li>
+                </ul>
+              </div>
+
+              <div class="col">
+                <h4>SITES</h4>
+                <ul class="list-unstyled">
+                  <li>DC</li>
+                  <li>MAD Magazines</li>
+                  <li>DC Kids</li>
+                  <li>DC Universe</li>
+                  <li>DC Power Visa</li>
+                </ul>
+              </div>
+
+            </div>
+          </div>
+
+          <div>
+            <p>
+              All Site Content TM and &copy; 2020 DC Entertainement, unless otherwise <a href="">noted here</a>.
+              All right reserved.
+              <br>
+              <a href="">Cookies Settings</a>
+            </p>
+          </div>
+
+        </div>
+
+        <div class="col-6">
+          <img src="{{Vite::asset('resources/img/dc-logo-bg.png')}}" alt="">
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+  <div id="footer_bottom">
+
+  </div>
 
 </footer>
