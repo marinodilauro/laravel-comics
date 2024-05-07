fd865de1abc3a03da25605993265e5fed4d6b182
marinodilauro <m.dilauro90@gmail.com>
2024-05-07 17:16:01 +0200

    Add style to comics list section. Add 'load more' button.

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -6,11 +6,11 @@
 <div id="comics_list">
     <section class="py-4">
         <div class="container">
-            <h2 class="text-white">CURRENT SERIES</h2>
+            <h1 id="page_title">CURRENT SERIES</h1>
             <div class="row row-cols-1 row-cols-md-4 row-cols-lg-6 g-5">
 
                 @foreach($comics as $comic)
-                <div class="col">
+                <div class="col mt-3">
                     <div class="comic border-0">
 
                         <img class="img-flud" src="{{$comic['thumb']}}" alt="Cover image of {{$comic['title']}}">
@@ -25,6 +25,11 @@
 
             </div>
         </div>
+
+        <div class="d-flex justify-content-center align-items-center mt-5">
+            <button type="button" class="btn rounded-0 load_more_btn py-2">LOAD MORE</button>
+        </div>
+
     </section>
 </div>
 
